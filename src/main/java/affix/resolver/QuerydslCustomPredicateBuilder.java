0946e61c5ef090a6ccecb4c54d6f0bde75b63c98
getouo <soliangsx@hotmail.com>
2019-02-28 18:22:37 +0800

    published

--- src/main/java/affix/resolver/QuerydslCustomPredicateBuilder.java
+++ src/main/java/affix/resolver/QuerydslCustomPredicateBuilder.java
@@ -1,18 +1,21 @@
 package affix.resolver;
 
 import affix.binding.QuerydslExpandBinding;
+import affix.binding.QuerydslLikeBinding;
 import com.querydsl.core.types.Path;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.data.querydsl.EntityPathResolver;
+import org.springframework.data.querydsl.binding.MultiValueBinding;
 import org.springframework.data.querydsl.binding.QuerydslPredicateBuilder;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 public class QuerydslCustomPredicateBuilder extends QuerydslPredicateBuilder {
-
+    private MultiValueBinding<Path<? extends Object>, Object> defaultBindings;
     /**
      * Creates a new {@link QuerydslCustomPredicateBuilder} for the given {@link ConversionService} and
      * {@link EntityPathResolver}.
@@ -25,6 +28,7 @@ public class QuerydslCustomPredicateBuilder extends QuerydslPredicateBuilder {
         customBinding.ifPresent(binding -> {
             try {
                 final Field defaultBindingField = QuerydslPredicateBuilder.class.getDeclaredField("defaultBinding");
+//                Arrays.asList(QuerydslPredicateBuilder.class.getDeclaredFields()).stream().filter(field -> MultiValueBinding.class.isAssignableFrom(field.getType()))
                 defaultBindingField.setAccessible(true);
                 defaultBindingField.set(this, binding);
             } catch (NoSuchFieldException | IllegalAccessException e) {
